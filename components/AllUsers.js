1993be1ccac43296705d096495d79a8deb667921
Seth Caparelli <sethcaparelli@gmail.com>
2018-05-06 13:55:01 -0600

    all user info dropdown done

--- components/AllUsers.js
+++ components/AllUsers.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import { View, Text, StyleSheet, Image, ScrollView, Alert, TouchableOpacity } from "react-native"
 import { ListItem, Avatar } from "react-native-elements"
+import Friend from "./Friend"
 
 export default class AllUsers extends Component {
     constructor(props) {
@@ -16,7 +17,6 @@ export default class AllUsers extends Component {
       }
 
     componentDidMount() {
-        console.log(this.state.users)
         fetch("http://localhost:3000/users")
         .then(response => response.json())
         .then(users => {
@@ -25,81 +25,18 @@ export default class AllUsers extends Component {
         .catch(error => console.log(error))
     }
 
-    alertAddUser = (user) => {
-        Alert.alert(
-            user.name,
-            'Add as Friend',
-            [
-              {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
-              {text: 'OK', onPress: () => this.postFriend(user)},
-            ],
-            { cancelable: true }
-          )
-    }
-
-    postFriend = (user) => {
-        const friend = user
-        console.log("postuser:", user)
-        const id = this.state.currentUser.fbId
-        console.log(id)
-        fetch(`http://localhost:3000/users/${id}`)
-        .then(response => response.json())
-        .then(currentUser => {
-            const sameFriend = currentUser.friends.filter(friend => {
-                return friend._id == user._id
-            })
-            // console.log("postUser:", user)
-            console.log("sameFriend:", sameFriend)
-            if(sameFriend.length > 0) {
-                Alert.alert(
-                    `${user.name} is already your friend`,
-                    "",
-                    [
-                    {text: "Cancel", style: "cancel"},
-                    {text: "OK"},
-                    ],
-                    {cancelable: true}
-                    )
-            } else {
-                // console.log(id)
-                this.state.currentUser.friends.push(friend)
-                console.log(this.state.currentUser)
-                // debugger
-                fetch(`http://localhost:3000/users/${id}`, {
-                    method: "PUT",
-                    headers: {
-                        "Accept": "application/json",
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify(this.state.currentUser)
-                    })
-                .then(response => console.log(response))
-                .catch(error => console.log())
-            }
-        })
-        .catch(error => console.log(error))
-    }
-debugger
   render() {
     return (
         <ScrollView>
-        <View style={styles.container}>
-            {
-                this.state.users.map((user) => {
-                    return (
-                        <TouchableOpacity
-                            key={user._id}
-                            onPress={() => this.alertAddUser(user)}
-                            style={styles.card}>
-                            <Image
-                                style={{width: 75, height: 75, borderRadius: "40%"}}
-                                source={{uri: user.picture.data.url}}/>
-                            <Text style={{width: "100%", textAlign: "center"}}>{user.name}</Text>
-                        </TouchableOpacity>
-                    )
-                })
-            }
-        </View>
+            <View style={styles.container}>
+                {
+                    this.state.users.map((user) => {
+                        return (
+                            <Friend friend={user}/>
+                        )
+                    })
+                }
+            </View>
       </ScrollView>
     )
   }
@@ -110,7 +47,7 @@ const styles = StyleSheet.create({
         flex: 1,
         // justifyContent: "center",
         // alignItems: "center",
-        backgroundColor: "black"
+        backgroundColor: "white"
     },
     card: {
         justifyContent: "flex-start",
---
+++ components/Friend.js
@@ -0,0 +1,149 @@
+import React, { Component } from 'react'
+import { View, StyleSheet, TouchableOpacity, Text } from "react-native"
+import { Avatar } from "react-native-elements"
+import { Icon } from "native-base"
+import renderIf from "../assets/functions/renderIf"
+
+export default class Friend extends Component {
+    constructor(props) {
+        super(props)
+        this.state ={
+            currentUser: this.props.currentUser,
+            friend: this.props.friend,
+            infoVisible: false
+        }
+    }
+
+    alertAddUser = (user) => {
+        Alert.alert(
+            user.name,
+            'Add as Friend',
+            [
+              {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+              {text: 'OK', onPress: () => this.postFriend(user)},
+            ],
+            { cancelable: true }
+          )
+    }
+
+    postFriend = (user) => {
+        const friend = user
+        const id = this.state.currentUser.fbId
+        fetch(`http://localhost:3000/users/${id}`)
+        .then(response => response.json())
+        .then(currentUser => {
+            const sameFriend = currentUser.friends.filter(friend => {
+                return friend._id == user._id
+            })
+            // console.log("postUser:", user)
+            console.log("sameFriend:", sameFriend)
+            if(sameFriend.length > 0) {
+                Alert.alert(
+                    `${user.name} is already your friend`,
+                    "",
+                    [
+                    {text: "Cancel", style: "cancel"},
+                    {text: "OK"},
+                    ],
+                    {cancelable: true}
+                    )
+            } else {
+                this.state.currentUser.friends.push(friend)
+                console.log(this.state.currentUser)
+                fetch(`http://localhost:3000/users/${id}`, {
+                    method: "PUT",
+                    headers: {
+                        "Accept": "application/json",
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(this.state.currentUser)
+                    })
+                .then(response => response.json())
+                .then(user => {
+                    this.setState({
+                        currentUser: user
+                    })
+                    Alert.alert(
+                        "Success",
+                        `${friend.name} is now your friend`,
+                        [
+                        {text: "OK"},
+                        ],
+                        {cancelable: false}
+                    )
+                })
+                .catch(error => console.log())
+            }
+        })
+        .catch(error => console.log(error))
+    }
+
+    toggleInfo = () => {
+        this.setState({infoVisible: !this.state.infoVisible})
+    }
+
+  render() {
+      const friend = this.state.friend
+    return (
+        <View>
+            <TouchableOpacity
+                style={styles.card}
+                key={friend._id}
+                onPress={() => this.toggleInfo()}
+                >
+                <Avatar
+                    medium
+                    rounded
+                    source={{uri: friend.picture.data.url}}
+                    onPress={() => console.log("Works!")}
+                    activeOpacity={0.7}
+                />
+                <Text>{friend.name}</Text>
+            </TouchableOpacity>
+            {renderIf(this.state.infoVisible)(
+            <View style={styles.userInfo}>
+                <View>
+                    <Text>Add Friend</Text>
+                    <Icon name='home' />
+                </View>
+                <View>
+                    <Text>Friends</Text>
+                    {friend.friends.map((user) => {
+                       return(
+                           <View style={styles.user}>
+                            <Text>{user.name}</Text>
+                            {/* <Image source={require("../assets/icons/add_icon.png")}/> */}
+                        </View>
+                       )
+                    })}
+                </View>
+            </View>)}
+        </View>
+
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "black"
+    },
+    card: {
+        flex: 1,
+        width: "100%",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: "black",
+        marginTop: 1,
+        marginBottom: 1,
+        backgroundColor: "white"
+   },
+//    userInfo: {
+//        display: this.state.infoVisible
+//    }
+})
